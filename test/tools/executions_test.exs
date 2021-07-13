9ff339c74b415352977a141c556325f8b80fa34c
drd-2rcl <drd.rclrd@gmail.com>
2021-07-13 09:15:57 -0300

    Created test to executions

---
+++ test/tools/executions_test.exs
@@ -0,0 +1,22 @@
+defmodule Tools.ExecutionsTest do
+  use ExUnit.Case
+
+  import ExUnit.CaptureIO
+
+  alias Tools.Executions
+
+  describe "exec/2" do
+    test "when the function receive values to execute" do
+      x = 5
+      y = 5
+      rover_position = "12N"
+      rover_commands = "LMLMLMLMM"
+      rover_position_2 = "33E"
+      rover_commands_2 = "MMRMMRMRRM"
+
+      result = capture_io(fn -> Executions.exec([x, y], [[rover_position, rover_commands], [rover_position_2, rover_commands_2]]) end)
+
+      assert result = "13N"
+    end
+  end
+end
