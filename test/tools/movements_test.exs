3ae2b715c3abbfc8f9053a022d570b9af9a7527c
drd-2rcl <drd.rclrd@gmail.com>
2021-07-13 19:26:07 -0300

    Added test to movements and principal module

--- test/tools/movements_test.exs
+++ test/tools/movements_test.exs
@@ -1,4 +1,4 @@
-defmodule ExploringMarsTest do
+defmodule Tools.MovementsTest do
   use ExUnit.Case
 
   import ExUnit.CaptureIO
@@ -6,17 +6,17 @@ defmodule ExploringMarsTest do
   alias Tools.Executions
 
   describe "exec/2" do
-    test "when execute all modules" do
+    test "when the function receive values to move" do
       x = 5
       y = 5
-      rover_position = "34E"
-      rover_commands = "MMLRMLLMMR"
-      rover_position_2 = "34E"
-      rover_commands_2 = "MMLRMLLMMR"
+      rover_position = "12N"
+      rover_commands = "LMLMLMLMM"
+      rover_position_2 = "33E"
+      rover_commands_2 = "MMRMMRMRRM"
 
       result = capture_io(fn -> Executions.exec([x, y], [[rover_position, rover_commands], [rover_position_2, rover_commands_2]]) end)
 
-      assert result = "44N"
+      assert result = "51E"
     end
   end
 end
