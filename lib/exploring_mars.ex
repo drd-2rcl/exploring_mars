93365b96ce47acd753452a22b7f31828e132c9ed
drd-2rcl <drd.rclrd@gmail.com>
2021-07-13 09:51:24 -0300

    Join the values of the modules to execute

--- lib/exploring_mars.ex
+++ lib/exploring_mars.ex
@@ -1,18 +1,14 @@
 defmodule ExploringMars do
-  @moduledoc """
-  Documentation for `ExploringMars`.
-  """
+  alias Tools.{PosDir, Executions}
 
-  @doc """
-  Hello world.
+  def call do
+    x = PosDir.get_x()
+    y = PosDir.get_y()
+    rover_position = PosDir.get_position()
+    rover_commands = PosDir.get_commands()
+    rover_position_2 = PosDir.get_position()
+    rover_commands_2 = PosDir.get_commands()
 
-  ## Examples
-
-      iex> ExploringMars.hello()
-      :world
-
-  """
-  def hello do
-    :world
+    Executions.exec([x, y], [[rover_position, rover_commands], [rover_position_2, rover_commands_2]])
   end
 end
